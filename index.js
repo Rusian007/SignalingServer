5290d0227ca179fab0d293463d0680389b01e308
Rusian007 <arafat.rahman19@outlook.com>
2023-11-18 16:02:30 +0600

    Peer fixed

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const path = require('path');
-const { createServer } = require('http');
+// const path = require('path');
+// const { createServer } = require('http');
 
-const express = require('express');
-const { getIO, initIO } = require('./socket');
+// const express = require('express');
+// const { getIO, initIO } = require('./socket');
 
 // const app = express();
 
 // app.use('/', express.static(path.join(__dirname, 'static')));
 
-// const httpServer = createServer(app);
+//const httpServer = createServer(app);
 
 // let port = process.env.PORT || 3500;
 
-const { PeerServer } = require("peer");
+require('dotenv').config()
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
 
-const peerServer = PeerServer({ port: 9000, path: "/myapp" });
+const app = express();
+
+app.get("/", (req, res, next) => res.send("Hello world!"));
+
+const http = require("http");
+
+const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: "/",
+});
+
+app.use("/peerjs", peerServer);
+
+server.listen(process.env.PORT || 9000);
 
 // initIO(httpServer);
 
