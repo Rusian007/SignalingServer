a948e3a9ec3c4a38f956b7d6f9a03a7e3a50b1db
Rusian007 <arafat.rahman19@outlook.com>
2023-11-18 21:57:10 +0600

    Server code added

--- socket.js
+++ socket.js
@@ -15,11 +15,30 @@ module.exports.initIO = (httpServer) => {
   IO.on("connection", (socket) => {
     console.log(socket.user, "Connected");
     socket.join(socket.user);
+    
+  /****************************For chat */
+  socket.on("chatSend", (data) => {
+    let senderID = socket.user;
+    let receiverID = data.receiverID;
+    let senderType=data.senderType;
+    let chatMessage = data.chatMessage;
 
+    
+    socket.to(receiverID).emit("chatReceived", {
+      chatMessage,
+      senderID,
+      receiverID,
+      senderType
+    });
+  });
+
+  /****************************End chat */
     socket.on("call", (data) => {
       let calleeId = data.calleeId;
       let rtcMessage = data.rtcMessage;
-
+      console.log(data, "Call");
+      console.log("rtcMessage",rtcMessage );
+      
       socket.to(calleeId).emit("newCall", {
         callerId: socket.user,
         rtcMessage: rtcMessage,
@@ -30,17 +49,33 @@ module.exports.initIO = (httpServer) => {
       let callerId = data.callerId;
       rtcMessage = data.rtcMessage;
 
+      console.log("answerCall" );
+      console.log("rtcMessage",rtcMessage );
       socket.to(callerId).emit("callAnswered", {
         callee: socket.user,
         rtcMessage: rtcMessage,
       });
     });
 
+    socket.on("endCall", (data) => {
+      let callerId = data.callerId;
+      rtcMessage = data.callEnd;
+
+      
+      socket.to(callerId).emit("callEnd", {
+        callee: socket.user,
+        rtcMessage: rtcMessage,
+      });
+    });
+
+
+
     socket.on("ICEcandidate", (data) => {
       console.log("ICEcandidate data.calleeId", data.calleeId);
       let calleeId = data.calleeId;
       let rtcMessage = data.rtcMessage;
-
+      console.log("socket.user emit", socket.user);
+      console.log("socket.user emit", rtcMessage);
       socket.to(calleeId).emit("ICEcandidate", {
         sender: socket.user,
         rtcMessage: rtcMessage,
